12c30cbc67fbe092a587a7f206a57500abe9add3
Rohan Bhande <rbhande@hawk.iit.edu>
2024-11-11 14:55:37 -0600

    added basic flutter files

---
+++ android/app/src/main/kotlin/com/example/uni_quest/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.uni_quest
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(title: 'Scaffold'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  final String? title;
+  MyHomePage({super.key, required this.title});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title ?? ''),
+      ),
+      body: Center(
+        child: Text('Welcome to the Flutter Demo Home Page!'),
+      ),
+    );
+  }
+}
